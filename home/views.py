34e236323b11ea4f86242d3562f82e2094683d6e
snigdho48 <www.atik48@gmail.com>
2021-06-02 13:44:45 +0600

    some ui fix from all pages

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib import messages
+
 
 
 # Create your views here.
 
 
 def index(request):
+    print(f'{messages}')
     return render(request, 'home/index.html')
